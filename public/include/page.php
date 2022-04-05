f1cd8a7badfbc5c8886ff63e16e19c483164bb1c
alfadila satriatama <alfadilasatriatama@gmail.com>
2022-04-05 13:17:23 +0700

    upload file port firsttime

---
+++ public/include/page.php
@@ -0,0 +1,26 @@
+<?php 
+
+@$page = $_GET['page'];
+      if($page) {
+        switch ($page) {
+          case 'about':
+            include "page/about.php";
+            break;
+          
+          case 'experience': 
+            include "page/experience.php";
+            break;
+          
+          case 'contact': 
+            include "page/contact.php";
+            break;
+          
+          case 'mygallery': 
+            include "page/mygallery.php";
+            break;         
+        }
+      }elseif($_POST) {
+        include "page/send.php";
+      }else {
+        include "page/about.php";
+      }
